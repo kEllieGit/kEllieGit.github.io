8260901f333159aea1153ced8d2cf394880c6e3e
kEllie <kEllieBusiness2@gmail.com>
2024-12-20 02:44:05 +0200

    Remove the other pages for now (they're shit)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Copyright from './components/Copyright'
+import Copyright from './components/Copyright';
+import SeasonManager from './components/SeasonManager';
+import Home from './pages/Home';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
@@ -9,9 +12,10 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
-        <>
-            <Navbar />
+        <Router>
+            <SeasonManager />
+            <Home />
             <Copyright />
-        </>
+        </Router>
     );
 }
