4b9c6c0e0fbcc90db197568a8bdc6baedf86490d
kEllie <kEllieBusiness2@gmail.com>
2025-01-14 21:51:45 +0200

    Update copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Copyright: React.FC = () => {
     return (
         <div className='copyright'>
-            <p>© <strong>kEllieDev</strong> 2024</p>
+            <p>© <strong>kEllieDev</strong> 2025</p>
         </div>
     );
 }
