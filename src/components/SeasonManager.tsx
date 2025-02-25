de260d3b5e640b6f29e590a6100e110aad0eb503
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-02-25 18:26:56 +0200

    We only use these here

--- src/components/SeasonManager.tsx
+++ src/components/SeasonManager.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import SnowfallEffect from './Snowfall';
-import { Season, getSeason } from "../util/Seasons";
 import { useLocation } from "react-router-dom";
 
 export const SeasonManager: React.FC = () => {
     const location = useLocation();
+    
+    enum Season {
+        Spring,
+        Summer,
+        Fall,
+        Winter
+    }
+
+    /**
+    * Returns the currently active season.
+    */
+    const getSeason = (): Season => {
+        const month = new Date().getMonth();
+
+        switch (true) {
+            case month >= 2 && month <= 4:
+                return Season.Spring;
+            case month >= 5 && month <= 7:
+                return Season.Summer;
+            case month >= 8 && month <= 10:
+                return Season.Fall;
+            default:
+                return Season.Winter;
+        }
+    };
+
     const season: Season = getSeason();
 
     // Pages where seasonal effects shouldn't play.
