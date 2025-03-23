a78b93f24ed40d706f8a460dd90fbe1e066a4215
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-03-23 16:03:27 +0200

    Add link to website source code in copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -4,6 +4,7 @@ const Copyright: React.FC = () => {
 	return (
 		<div className='copyright'>
 			<p>© <strong>kEllieDev</strong> 2025</p>
+			<p>Visit the source code <a href='https://github.com/kEllieGit/kEllieGit.github.io'>here!</a></p>
 		</div>
 	);
 }
